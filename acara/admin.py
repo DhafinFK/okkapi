553cc1012a700fc8fa74bcd255095e0e6a83f042
DhafinFK <dhafin.kamal@gmail.com>
2024-03-01 05:46:47 +0700

    altered pembicara and sponsor

--- acara/admin.py
+++ acara/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Acara
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Acara)
+admin.site.register(Sponsor)
+admin.site.register(SponsorAcara)
+admin.site.register(Pembicara)
+admin.site.register(PembicaraMateri)
+admin.site.register(Materi)
--- acara/models.py
+++ acara/models.py
@@ -1,12 +1,61 @@
 from django.db import models
 from common.models import BaseModel
 
-# Create your models here.
+
+class Sponsor(BaseModel):
+    nama = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return f'sponsor - {self.nama}'
+    
 
 class Acara(BaseModel):
     nama = models.CharField(max_length=200, unique=True)
     waktu_mulai = models.DateTimeField(help_text="Tanggal dan waktu mulai acara")
     waktu_selesai = models.DateTimeField(help_text="Tanggal dan waktu selesai acara")
+    sponsor_list = models.ManyToManyField(Sponsor, through='SponsorAcara')
 
     def __str__(self):
         return (self.nama)
+    
+
+class SponsorAcara(BaseModel):
+    PACKAGE_CHOICES = [
+        ('Platinum', 'Platinum'),
+        ('Gold', 'Gold'),
+        ('Silver', 'Silver'),
+    ]
+
+    sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE)
+    acara = models.ForeignKey(Acara, on_delete=models.CASCADE)
+    package = models.CharField(max_length=200, choices=PACKAGE_CHOICES)
+
+    def __str__(self):
+        return f'Sponsor {self.sponsor.nama} paket {self.package} untuk {self.acara.nama}'
+    
+    
+    class Meta:
+        unique_together = ['sponsor', 'acara']
+    
+
+class Materi(BaseModel):
+    acara = models.ForeignKey(Acara, on_delete=models.CASCADE)
+    judul = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f'Pembicara {self.pembicara} untuk acara {self.acara.nama}'
+    
+
+class Pembicara(BaseModel):
+    nama = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.nama}'
+
+
+class PembicaraMateri(BaseModel):
+    pembicara = models.CharField(max_length=200)
+    materi = models.ForeignKey(Materi, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'pembicara - {self.pembicara}'
